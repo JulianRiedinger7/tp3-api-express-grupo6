ff6de81a00581555c621b4055b6c582c62468a1c
Julian Riedinger <julianriedinger27@gmail.com>
2026-05-10 11:25:48 -0300

    feat: creacion archivos base y modelo equipo

---
+++ models/EquipoModel.js
@@ -0,0 +1,23 @@
+class EquipoModel {
+  constructor (id, nombre, apellido, rol, imagen, acercaDe) {
+    this.id = id
+    this.nombre = nombre
+    this.apellido = apellido
+    this.rol = rol
+    this.imagen = imagen
+    this.acercaDe = acercaDe
+  }
+
+  static getIntegranteDeJson (objeto) {
+    return new EquipoModel(
+      objeto.id,
+      objeto.nombre,
+      objeto.apellido,
+      objeto.rol,
+      objeto.imagen,
+      objeto.acercaDe
+    )
+  }
+}
+
+module.exports = EquipoModel
