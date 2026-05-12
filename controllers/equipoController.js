8323d7a9a47b724d4655c79b739d0105cb6ce46c
Julian Riedinger <julianriedinger27@gmail.com>
2026-05-12 10:13:36 -0300

    feat: busqueda de datos por modelo

--- controllers/equipoController.js
+++ controllers/equipoController.js
@@ -1,25 +1,25 @@
 const path = require('path')
 const EquipoModel = require('../models/EquipoModel')
-const { getJson } = require('../utils/funciones')
 const RUTA_JSON_EQUIPO = path.join(__dirname, '../data/equipo.json')
 
 const getEquipo = async (req, res) => {
   try {
-    const equipo = await getJson(RUTA_JSON_EQUIPO)
+    const integrantes = await EquipoModel.obtenerEquipo(RUTA_JSON_EQUIPO)
 
-    if (equipo.length === 0 || Object.keys(equipo).length === 0) {
-      res.status(400).json({ mensaje: 'No se encontraron integrantes del equipo', equipo })
+    if (integrantes.length === 0 || Object.keys(integrantes).length === 0) {
+      return res
+        .status(404)
+        .json({ mensaje: 'No se encontraron integrantes del equipo', equipo: [] })
     }
-    const integrantes = equipo.map((integrante) => EquipoModel.getIntegranteDeJson(integrante))
     console.log(
       new Date().toLocaleString() +
         ' - Integrantes del equipo parseados: ' +
         JSON.stringify(integrantes)
     )
-    res.status(200).json({ equipo: integrantes })
+    return res.status(200).json({ equipo: integrantes })
   } catch (error) {
     console.error('Error al obtener el equipo:', error)
-    res.status(500).json({ mensaje: 'Error interno del servidor', equipo: [] })
+    return res.status(500).json({ mensaje: 'Error interno del servidor', equipo: [] })
   }
 }
 
